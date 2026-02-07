6e025503eacd5c191b7ebe469897938fc8817506
sibasish005 <sibasishchakraborti@gmail.com>
2026-02-07 17:13:08 +0530

    seo changed

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/navbar'
 import Application from '../components/application'
+import Footer from '../components/footer'
 
 export default function Page() {
   return (
     <div>
       <Navbar/>
       <Application/>
+      <Footer/>
     </div>
   )
 }
